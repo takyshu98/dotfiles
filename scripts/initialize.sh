8bea83dfb701d893f77c88ecf892965de780f1e2
takyshu98 <shuhei.takiguchi.0908@gmail.com>
2020-01-25 00:35:04 +0900

    modify anyenv install envs condition

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -8,6 +8,14 @@ if [ ! -d "$HOME/.config/anyenv/anyenv-install" ]; then
   anyenv install --init
 fi
 
-anyenv install jenv
-anyenv install nodenv
-anyenv install pyenv
+if [ ! -d "$HOME/.anyenv/envs/jenv" ]; then
+  anyenv install jenv
+fi
+
+if [ ! -d "$HOME/.anyenv/envs/nodenv" ]; then
+  anyenv install nodenv
+fi
+
+if [ ! -d "$HOME/.anyenv/envs/pyenv" ]; then
+  anyenv install pyenv
+fi
