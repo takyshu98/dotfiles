67adc195c95d7a1e3cf52a531c1e52ae2d7dbc2b
takyshu98 <shuhei.takiguchi.0908@gmail.com>
2026-05-03 18:16:06 +0900

    WIP

--- nix/darwin/configuration.nix
+++ nix/darwin/configuration.nix
@@ -16,7 +16,6 @@ in
         "com.apple.trackpad.scaling" = 3;
         "com.apple.mouse.scaling" = 3;
         "com.apple.scrollwheel.scaling" = 1;
-        "com.apple.mouse.tapBehavior" = 1;
       };
     };
 
@@ -36,6 +35,15 @@ in
 
     spaces.spans-displays = true;
   };
+
+  # Run tap-to-click settings as the user since defaults write is user-specific
+  system.activationScripts.postActivation.text = ''
+    /usr/bin/su -l ${username} -c '
+      defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+      defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+      defaults -currentHost write -g com.apple.mouse.tapBehavior -bool true
+    '
+  '';
 
   system.primaryUser = username;
   system.stateVersion = 6;
